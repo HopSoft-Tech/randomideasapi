2bbc6e7c60a42f37ce05410f2fcd35e147abf001
HopSoft-Tech <ndukelex4@gmail.com>
2026-03-31 21:09:49 +0200

    Put and Delete Requests, Update and Delete Ideas

--- routes/ideas.js
+++ routes/ideas.js
@@ -56,4 +56,48 @@ router.post("/", (req, res) => {
   res.json({ success: true, data: idea });
 });
 
+// Update Idea
+router.put("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === parseInt(req.params.id));
+
+  if (!idea)
+    return res
+      .status(404)
+      .json({ success: false, message: "Could not find idea with that id" });
+
+  idea.text = req.body.text || idea.text;
+  idea.tag = req.body.tag || idea.tag;
+
+  res.json({ success: true, data: idea });
+});
+
+// Delete Idea
+// router.delete("/:id", (req, res) => {
+//   const index = ideas.findIndex((idea) => idea.id === parseInt(req.params.id));
+
+//   if (index === -1)
+//     return res
+//       .status(404)
+//       .json({ success: false, message: "Could not find idea with that id" });
+
+//   // Remove the Idea based on the index
+//   ideas.splice(index, 1);
+
+//   res.json({ success: true, data: ideas });
+// });
+
+// OR use this to Delete Idea
+router.delete("/:id", (req, res) => {
+  const idea = ideas.find((idea) => idea.id === parseInt(req.params.id));
+
+  if (!idea)
+    return res
+      .status(404)
+      .json({ success: false, message: "Could not find idea with that id" });
+
+  const index = ideas.indexOf(idea);
+  ideas.splice(index, 1);
+  res.json({ success: true, data: {} });
+});
+
 module.exports = router;
